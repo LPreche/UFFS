f51a58e95b6da0150bced19f4cff751a514673c4
Luiz Paulo <paulorecheme@hotmail.com>
2022-06-28 00:36:32 -0300

    Update exerc2.asm

--- OrganizacaoDeComputadores/Atividade03/exerc2.asm
+++ OrganizacaoDeComputadores/Atividade03/exerc2.asm
@@ -6,23 +6,37 @@ vetor2: .word	-10, 8, 11, 16, 15, -6, 22
 	.text
 	
 main:
+	addi	t0,t0,4
 	la 	a0, vetor1
 	li 	a1, 8
 	li 	a2, 0
+	mul	a3,a0,t0
+	mul	a4,a0,t0
 	jal	shift
 	
 	la 	a0, vetor2
 	li 	a1, 5
 	li 	a2, 1
+	addi 	a3,a0,4
 	jal	shift
-
+	
 	li 	a7, 10   # chamada de sistema para encerrar programa
 	ecall 
 	
 #####################################
-# Implemente a função abaixo	
+# Implemente a função abaixo	l=m
+
 
 shift:
-
+	bne a3,a4,troca
+	
 	ret
 		
+troca:
+	lw t1,(a3)
+	lw t2,(a4)
+	
+	sw t1,(a3)
+	sw t2,(a2)
+	
+	
