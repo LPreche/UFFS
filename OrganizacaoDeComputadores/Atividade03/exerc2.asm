db42e13b5ea1b78c28cb51ad6f096b545d7f3b6f
Luiz Paulo <paulorecheme@hotmail.com>
2022-06-29 00:53:19 -0300

    Finalização Exerc2

--- OrganizacaoDeComputadores/Atividade03/exerc2.asm
+++ OrganizacaoDeComputadores/Atividade03/exerc2.asm
@@ -6,18 +6,19 @@ vetor2: .word	-10, 8, 11, 16, 15, -6, 22
 	.text
 	
 main:
-	addi	t0,t0,4
+	addi 	t0,t0,4
 	la 	a0, vetor1
 	li 	a1, 8
 	li 	a2, 0
-	mul	a3,a0,t0
-	mul	a4,a0,t0
+	mul	t1,t0,a1
+	add	a3,a0,t1
 	jal	shift
 	
 	la 	a0, vetor2
 	li 	a1, 5
 	li 	a2, 1
-	addi 	a3,a0,4
+	mul	t1,t0,a1
+	add	a3,a0,t1
 	jal	shift
 	
 	li 	a7, 10   # chamada de sistema para encerrar programa
@@ -28,15 +29,22 @@ main:
 
 
 shift:
-	bne a3,a4,troca
+
+	bne	a1,a2,troca
 	
 	ret
 		
 troca:
-	lw t1,(a3)
-	lw t2,(a4)
+	addi	a4,a3,-4
+	lw	t1,(a3)
+	lw	t2,(a4)
 	
-	sw t1,(a3)
-	sw t2,(a2)
+	sw	t1,(a4)
+	sw	t2,(a3)
+	
+	addi	a3,a3,-4
+	addi	a1,a1,-1
+	
+	j shift
 	
 	
