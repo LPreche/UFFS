563fbc84c14c706c0a541ba1c324ce273702ddba
Luiz Paulo <paulorecheme@hotmail.com>
2022-07-01 00:41:17 -0300

    Finalização Exerc3

--- OrganizacaoDeComputadores/Atividade03/exerc3.asm
+++ OrganizacaoDeComputadores/Atividade03/exerc3.asm
@@ -24,6 +24,45 @@ main:
 #####################################
 # Implemente a função abaixo	
 ordena:
+	addi	t0,zero,4
+	add	a2,zero,zero
+	j	percorre
 
+percorre:
+	addi	a2,a2,1
+	add	t1,zero,a2
+	add	t4,zero,a2
+	bne	a2,a1,verifica
 	ret
-		
+	
+verifica:
+	
+	bne	t1,zero,localiza
+	j	percorre
+	
+	
+localiza:
+	addi	t1,t1,-1
+	mul	t2,t0,t1
+	mul	t3,t0,t4
+	add	a3,a0,t3
+	add	a4,a0,t2
+	lw	t5,(a3)
+	lw	t6,(a4)
+	bge	t6,t5,troca
+	
+	j	percorre
+	
+	
+troca:	
+	sw	t5,(a4)
+	sw	t6,(a3)
+	addi	t4,t4,-1
+	j	verifica
+	
+	
+	
+	
+	
+	
+	
