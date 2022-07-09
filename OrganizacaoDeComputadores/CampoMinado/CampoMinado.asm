cd69073d779ccab09093bafb11281c8af9cd18a8
Luiz Paulo Reche <paulorecheme@hotmail.com>
2022-07-09 17:44:24 -0300

    Update CampoMinado.asm

--- OrganizacaoDeComputadores/CampoMinado/CampoMinado.asm
+++ OrganizacaoDeComputadores/CampoMinado/CampoMinado.asm
@@ -1,19 +1,20 @@
 		.data
 
-matriz:		.word	1,1,1,1,1,1,1,1
-		.word	1,1,1,1,1,1,1,1
-		.word	1,1,1,1,1,1,1,1
-		.word	1,1,1,1,1,1,1,1
-		.word	1,1,1,1,1,1,1,1
-		.word	1,1,1,1,1,1,1,1
-		.word	1,1,1,1,1,1,1,1
-		.word	1,1,1,1,1,1,1,1	
+matriz:		.word	0,0,0,0,0,0,0,0,0
+		.word	0,0,0,0,0,0,0,0,0
+		.word	0,0,0,0,0,0,0,0,0
+		.word	0,0,0,0,0,0,0,0,0
+		.word	0,0,0,0,0,0,0,0,0
+		.word	0,0,0,0,0,0,0,0,0
+		.word	0,0,0,0,0,0,0,0,0
+		.word	0,0,0,0,0,0,0,0,0
 tam:		.word
 
 
-msg:		.asciz  "Digite a opção correspondente ao tamanho da matriz desejada\n"
+msg:		.asciz  "Digite a opção correspondente ao tamanho da matriz desejada (1)8x8 (2)10x10 (3)12x12\nSua Opção:	"
 espaco:		.asciz	" "
 quebra:		.asciz	"\n"
+op_invalida:	.asciz	"Opção invalida, Digite Novamente:	"
 		.text
 		
 		
@@ -22,23 +23,28 @@ main:
 	la	a0,msg
 	li	a7,4
 	ecall
-	li	a7,5
-	ecall
-	la 	t1,tam
-	sw 	a0,(t1)
+	
+	jal	seleciona_tamanho
+	
 	li	t1,0
 	lw	t2,tam
 	la	a1,matriz
-	jal	inicia_campo
+	
 	jal	mostra_campo
 	
 	li	a7,10
 	ecall
 	
+seleciona_tamanho:
+	li	a7,5
+	ecall
+	la 	t1,tam
+	sw 	a0,(t1)
+	li	t1,1
+	beq 	a0,t1,select8
+	beq 	a0,t1,select10
+	beq 	a0,t1,select12
 	
-	
-inicia_campo:
-	beq	t1,t2,fim
 	
 mostra_campo:
 	beq	t3,t2,fim
@@ -47,22 +53,21 @@ mostra_campo:
 	lw	a0,(a1)
 	li	a7,1
 	ecall
+	
 	la	a0,espaco
 	li	a7,4
 	ecall
-	
+
 	addi	t1,t1,1
-	
 	addi	a1,a1,4
-	
 	j	mostra_campo
+	
 Avanca_coluna:
 	li	t1,0
 	addi	t3,t3,1	
 	la	a0,quebra
 	li	a7,4
 	ecall
-	
 	j	mostra_campo
 	
 
