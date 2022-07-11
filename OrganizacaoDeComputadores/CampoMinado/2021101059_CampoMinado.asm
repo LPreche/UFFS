877fe15618efe0b98ba226df396da5a92e748886
Luiz Paulo <paulorecheme@hotmail.com>
2022-07-11 01:09:36 -0300

    Finalização 1ª etapa

--- OrganizacaoDeComputadores/CampoMinado/2021101059_CampoMinado.asm
+++ OrganizacaoDeComputadores/CampoMinado/2021101059_CampoMinado.asm
@@ -15,6 +15,8 @@ msg:		.asciz  "Digite a opção correspondente ao tamanho da matriz desejada (1)8x
 espaco:		.asciz	" "
 quebra:		.asciz	"\n"
 op_invalida:	.asciz	"Opção invalida, Digite Novamente:	"
+traco:		.asciz	"-"
+
 		.text
 		
 		
@@ -25,33 +27,67 @@ main:
 	ecall
 	
 	jal	seleciona_tamanho
-	
+	la	t1,tam
+	sw	t6,(t1)
 	li	t1,0
-	lw	t2,tam
-	la	a1,matriz
 	
+	la	a1,matriz
+	lw	t2,tam
 	jal	mostra_campo
 	
 	li	a7,10
 	ecall
 	
+	
+	
+	
+	
+	
+	
 seleciona_tamanho:
 	li	a7,5
-	ecall
-	la 	t1,tam
-	sw 	a0,(t1)
+	ecall	
+	
 	li	t1,1
-	beq 	a0,t1,select8
-	beq 	a0,t1,select10
-	beq 	a0,t1,select12
+	li	t6,8
+	beq 	a0,t1,return
 	
+	li	t1,2
+	li	t6,9
+	beq 	a0,t1,return
 	
+	li	t1,3
+	li	t6,9
+	beq 	a0,t1,return
+	 
+	la	a0,op_invalida
+	li	a7,4
+	ecall
+	
+	j	seleciona_tamanho
+	
+		
 mostra_campo:
-	beq	t3,t2,fim
+	beq	t3,t2,return
 	beq	t1,t2,Avanca_coluna
-
+	
 	lw	a0,(a1)
+	beq	a0,zero,mostra_traco
+	
 	li	a7,1
+	ecall
+	
+	la	a0,espaco
+	li	a7,4
+	ecall
+
+	addi	t1,t1,1
+	addi	a1,a1,4
+	j	mostra_campo
+	
+mostra_traco:
+	la	a0,traco
+	li	a7,4
 	ecall
 	
 	la	a0,espaco
@@ -72,7 +108,7 @@ Avanca_coluna:
 	
 
 
-fim:
+return:
 	ret
 	
 	
