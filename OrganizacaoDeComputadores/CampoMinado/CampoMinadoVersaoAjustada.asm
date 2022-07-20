30e8d0dc67fa286ee771215b0b67d2e1d25bd9b6
Luiz Paulo <paulorecheme@hotmail.com>
2022-07-20 01:17:32 -0300

    Finalizado Ajustes
    *Ajuste função para selecionar tamanho
    *Ajuste função de inicializar campo
    *Ajustadas Convenções de chamada de função

--- OrganizacaoDeComputadores/CampoMinado/CampoMinadoVersaoAjustada.asm
+++ OrganizacaoDeComputadores/CampoMinado/CampoMinadoVersaoAjustada.asm
@@ -1,8 +1,7 @@
 		.data
 
-matriz:		.space	576
-tamanho:	.word
-
+campo:		.space	576
+tamanho:	.word	0
 
 msg:		.asciz  "  Campo Minado\n1- Campo de 8x8\n2- Campo de 10x10\n3- Campo de 12x12\nSua Opção: "
 espaco:		.asciz	" "
@@ -19,16 +18,19 @@ main:
 	
 	#seleciona tamanho do campo
 	jal	seleciona_tamanho
+	la	t1,tamanho
+	sw	a1,(t1)
 	
 	#Inicia Campo
-	la	a0,matriz
-	la	a1,tamanho
-	li	t1,0#linha
-	li	t2,0#coluna		
+	la	a0,campo
+	li	t1,0#Inicia linha
+	li	t2,0#Inicia coluna		
 	jal	inicia_campo
 	
+	#mostrar campo inicial
+	jal	mostra_campo
 	
-	#Fin do programa
+	#Fim do programa
 	li	a7,10
 	ecall
 	
@@ -39,34 +41,37 @@ seleciona_tamanho:
 	
 	li	t2,1
 	li	a1,8
-	beq	t1,t2,grava_tamanho
+	beq	t1,t2,retorna_tamanho
 	
 	li	t2,2
 	li	a1,10
-	beq	t1,t2,grava_tamanho
+	beq	t1,t2,retorna_tamanho
 	
 	li	t2,3
 	li	a1,12
-	beq	t1,t2,grava_tamanho
+	beq	t1,t2,retorna_tamanho
 	
 	la	a0,op_invalida
 	li	a7,4
 	ecall
 	
 	j	seleciona_tamanho
-	
-grava_tamanho:
-	la	t1,tamanho
-	sw	a1,(t1)
-	ret
+retorna_tamanho:
+	ret #retornando tamanho em a1
 	
 inicia_campo:
-	mv	a0,t1
-	
-	
+	sw	zero,(a0)
+	addi	a0,a0,4
+	addi	t1,t1,1	
+	bne	t1,a1,inicia_campo
+	li	t1,0
+	addi	t2,t2,1
+	bne	t2,a1,inicia_campo
 	
 	ret
 
+mostra_campo:
+	ret
 	
 	
 	
